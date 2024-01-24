b22dc5b059f34db119d26508b0ac5004b75a271c
tor-tiid-johansson-nti-johanneberg <118816706+tor-tiid-johansson-nti-johanneberg@users.noreply.github.com>
2024-01-24 09:18:03 +0100

    b

--- app.rb
+++ app.rb
@@ -5,13 +5,18 @@ require 'sqlite3'
 require 'bcrypt'
 
 enable :sessions
+## använd before do för att se om man är inloggad
 
-
+def connect_to_db(path)
+    db = SQLite3::Database.new(path)
+    db.results_as_hash = true
+    return db
+end
 
 get('/') do
-    slim(:"users/register")
+    slim(:"users/login")
 end
 
 get ('/login') do
-    slim(:"users/login")
+    slim(:"users/register")
 end
