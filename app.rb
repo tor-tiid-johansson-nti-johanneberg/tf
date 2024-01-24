33f64b13db1f035f193ca83597f7b3557c44b223
tor-tiid-johansson-nti-johanneberg <118816706+tor-tiid-johansson-nti-johanneberg@users.noreply.github.com>
2024-01-24 10:37:36 +0100

    nästan klar med inlogg, fixa dynamiska routes efter inlogg

--- app.rb
+++ app.rb
@@ -14,9 +14,55 @@ def connect_to_db(path)
 end
 
 get('/') do
-    slim(:"users/login")
+    slim(:"login")
 end
 
-get ('/login') do
-    slim(:"users/register")
+get ('/register') do
+    slim(:"register")
+end
+
+post ('/login') do
+    username = params[:username]
+    password = params[:password]
+
+    db = connect_to_db("db/db.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect('/users/:id')
+    else
+        "Password is incorrect!"
+    end
+end
+
+get ('/users/:id') do
+    id = session[:id].to_i
+    db = connect_to_db("db/db.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE id = ?", id).first
+end
+
+
+post ('/users/new') do
+
+    username = params[:username]
+    password = params[:password]
+    confirm_password = params[:confirm_password]
+
+
+    if password == confirm_password
+        ## lägg till användare i databasen
+        password_digest = BCrypt::Password.create(password)
+        db = connect_to_db("db/db.db")
+        db.execute("INSERT INTO users (username, pwdigest) VALUES (?, ?)", username, password_digest)
+        redirect('/')
+
+    else
+        ## felhantering
+        "Passwords don't match!"
+    end
 end
